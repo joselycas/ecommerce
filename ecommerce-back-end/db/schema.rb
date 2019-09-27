f9a890a73037f40ddb9acd4147cad71be6495db6
github username <github email address>
2019-09-27 09:06:12 -0400

    added products model

--- ecommerce-back-end/db/schema.rb
+++ ecommerce-back-end/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_232235) do
+ActiveRecord::Schema.define(version: 2019_09_27_130441) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.decimal "price"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
