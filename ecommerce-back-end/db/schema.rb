e2dead5541d6ec57815018e8a778b8dda7cb4a14
github username <github email address>
2019-09-30 10:12:41 -0400

    added category and product_category models

--- ecommerce-back-end/db/schema.rb
+++ ecommerce-back-end/db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_27_130441) do
+ActiveRecord::Schema.define(version: 2019_09_30_135250) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_product_categories_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
